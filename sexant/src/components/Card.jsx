1a77676938c19ccc4a08332c348ad17ff086bc59
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-07-02 11:08:39 +0000

    add card component

---
+++ sexant/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card({}) {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <header>Header</header>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, nesciunt?</p>
+      </div>
+    </div>
+  );
+}
